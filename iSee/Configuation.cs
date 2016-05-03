59c359a2454111ec62f855bcc8c0618a84cd1684
7Finn <wu7finn@gmail.com>
2016-05-03 22:34:37 +0800

    初始化

---
+++ iSee/Configuation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+namespace iSee
+{
+    public partial class MainPage : Page
+    {
+        public const string FEATURE_NAME = "Calendar C# sample";
+
+        List<Scenario> scenarios = new List<Scenario>
+        {
+            new Scenario() { Symbol = "Favorite", Title = "想看的", ClassType = typeof(iSee.BlankPage) },
+            new Scenario() { Symbol = "List", Title = "已看电影", ClassType = typeof(iSee.BlankPage1) },
+            new Scenario() { Symbol = "Like", Title = "私人影院", ClassType = typeof(iSee.BlankPage1) },
+            //new Scenario() { Title = "Calendar with Unicode extensions in languages", ClassType = typeof(SDKTemplate.Scenario4_UnicodeExtensions) },
+            //new Scenario() { Title = "Calendar time zone support", ClassType = typeof(SDKTemplate.Scenario5_TimeZone) }
+        };
+    }
+
+    public class Scenario
+    {
+        public string Symbol { get; set; }
+        public string Title { get; set; }
+        public Type ClassType { get; set; }
+    }
+}
---
+++ iSee/MainPage.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace iSee
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public static MainPage Current;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Current = this;
+            InitialzeTitleBar();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Populate the scenario list from the SampleConfiguration.cs file
+            ScenarioControl.ItemsSource = scenarios;
+            if (Window.Current.Bounds.Width < 640)
+            {
+                ScenarioControl.SelectedIndex = -1;
+            }
+            else
+            {
+                ScenarioControl.SelectedIndex = 0;
+            }
+        }
+
+        private void ScenarioControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Clear the status block when navigating scenarios.
+            NotifyUser(String.Empty, NotifyType.StatusMessage);
+
+            ListView scenarioListView = sender as ListView;
+            Scenario s = scenarioListView.SelectedItem as Scenario;
+            if (s != null)
+            {
+                ScenarioFrame.Navigate(s.ClassType);
+                if (Window.Current.Bounds.Width < 640)
+                {
+                    Splitter.IsPaneOpen = false;
+                }
+            }
+        }
+
+        public void NotifyUser(string strMessage, NotifyType type)
+        {
+            switch (type)
+            {
+                case NotifyType.StatusMessage:
+                    StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Green);
+                    break;
+                case NotifyType.ErrorMessage:
+                    StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                    break;
+            }
+            StatusBlock.Text = strMessage;
+
+            // Collapse the StatusBlock if it has no text to conserve real estate.
+            StatusBorder.Visibility = (StatusBlock.Text != String.Empty) ? Visibility.Visible : Visibility.Collapsed;
+            if (StatusBlock.Text != String.Empty)
+            {
+                StatusBorder.Visibility = Visibility.Visible;
+                StatusPanel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                StatusBorder.Visibility = Visibility.Collapsed;
+                StatusPanel.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        public List<Scenario> Scenarios
+        {
+            get { return this.scenarios; }
+        }
+
+
+        public void InitialzeTitleBar()
+        {
+            var viewTitleBar = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().TitleBar;
+            viewTitleBar.BackgroundColor = Color.FromArgb(255, 188, 47, 46);
+            viewTitleBar.ButtonBackgroundColor = Color.FromArgb(255, 188, 47, 46);
+            viewTitleBar.ButtonHoverBackgroundColor = Color.FromArgb(255, 232, 17, 35);
+            viewTitleBar.ButtonPressedBackgroundColor = Color.FromArgb(255, 241, 112, 122);
+            viewTitleBar.ButtonInactiveBackgroundColor = Color.FromArgb(255, 181, 113, 113);
+            viewTitleBar.InactiveBackgroundColor = Color.FromArgb(255, 181, 113, 113);
+            viewTitleBar.InactiveForegroundColor = Colors.White;
+            viewTitleBar.ButtonInactiveForegroundColor = Colors.White;
+            viewTitleBar.ButtonHoverForegroundColor = Colors.White;
+        }
+    }
+
+    public enum NotifyType
+    {
+        StatusMessage,
+        ErrorMessage
+    };
+
+    public class ScenarioBindingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            Scenario s = value as Scenario;
+            return s.Title;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return true;
+        }
+    }
+
+    public class ScenarioBindingConverterSymbol : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            Scenario s = value as Scenario;
+            return s.Symbol;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return true;
+        }
+    }
+}
